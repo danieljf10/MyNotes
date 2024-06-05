14814d04da284252e5444b4e4106a87ce0b4cd2e
danieljf10 <121913982+danieljf10@users.noreply.github.com>
2024-06-05 12:31:30 -0700

    Completed Notes App (I think)

--- App.js
+++ App.js
@@ -1,135 +1,158 @@
-import { SafeAreaView, Text, View, TextInput, TouchableOpacity} from 'react-native';
+// imported libraries & components to use in app
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, TextInput, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import 'react-native-reanimated'; 
-import { useState, useEffect } from 'react';
-import MasonryList from '@react-native-seoul/masonry-list';
+import MasonryList from '@react-native-seoul/masonry-list'; 
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
 
+// Main Home Screen of App
+function HomeScreen({ navigation }) {
+  const [searchText, setSearchText] =  useState(""); // searchText (useState Hook), setSearchText (update search text)
+  const { data: searchData, error, isLoading, } = useSearchNotesQuery(searchText); // destructuring return value
+  const [ addNote, { data: addNoteData, error: addNoteError }] = useAddNoteMutation(); // destructuring note data
+  const [deleteNote] = useDeleteNoteMutation();
 
+  useEffect(() => { // useEffect hook
+    if (addNoteData != undefined) {
+      navigation.navigate("Edit", { data:  addNoteData }); // navigate to edit screen & pass data
+    }
+  }, [addNoteData]);
 
-function HomeScreen(){
-  const [text, setText] = useState();
+    const renderItem = ({ item }) => ( // navigate to edit screen when user presses button
+      <TouchableOpacity onPress={() => navigation.navigate("Edit", { data: item })} style={tw`w-[99%] mb-2 bg-gray-300 rounded-sm px-2 py-1 justify-center`}>
+        <Text  style={tw`text-lg font-bold`}>{item.title}</Text>
+        <Text>{item.content}</Text> 
+      </TouchableOpacity>
+    );
 
-  
-  const [data, setData] = useState([]);
-  const generateData = (count) => Array.from({ length: count }, (_, i) => ({ id: (i + 1).toString() }));
+  return ( // Home page styling
+    <View style={tw`flex-1 bg-gray-900`}>
+      <TextInput
+        style={tw`w-auto justify-center p-2  m-2 bg-gray-800 text-white mb-2`}
+        placeholder="Search"
+        value={searchText}
+        onChangeText={setSearchText} // update text to search for
+        />
+      {searchData ? // if searchdata is available, display any matching text
+        <MasonryList
+          style= {tw`p-2`}
+          data={searchData}
+          numColumns={3}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id} // unique key for each note
+          showsVerticalScrollIndicator={false}
+        /> : <></>
+      }
 
-  useEffect(() => {
-    setData(generateData(100));
-  }, []);
-
-  const renderItem=({item, i}) => (
-    <Text style={[tw`bg-gray-900 text-gray-500 m-1`, { height: Math.floor(Math.random() * 100) + 50 }]}>
-      {item.id}
-    </Text>
-  )
-
-  const [on, setOn] = useState(false);
-  const callback = () => { !on ? setOn(true) : setOn(false) }
-
-  return (
-    
-    <View style={tw`w-full h-full`}>
-      <View>
-        <Text style={tw`text-white text-center text-3xl mt-18 mb-5 font-bold`}>Notes</Text>
-        <TextInput style={tw`w-100 h-8 bg-gray-800 rounded-lg text-white pl-2 my-1 m-auto`} placeholder="Search" value={text} onChangeText={(newValue) => setText(newValue)}/>
-        <Text style={tw`mt-3 mb-3 text-center text-gray-400`}>Search For: {text}</Text>
-      </View>
-      
-      <View style={tw`w-full h-1 bg-gray-600 `}></View>
-      <MasonryList 
-      style={tw`w-full h-screen mt-5 m-auto bg-gray-800`}
-      data={data}
-      keyExtractor={(item) => item.id}
-      numColumns={3}
-      showsVerticalScrollIndicator={false}
-      renderItem={renderItem}
-      onEndReachedThreshold={0.1}
-    />
-    
-    <View style={tw`w-auto h-auto absolute bottom-4 right-4`}>
-      <TouchableOpacity style={tw`w-24 h-24  mb-1 text-2xl mx-auto mt-30 ${on ? "bg-green-900" : "bg-blue-900"} rounded-full`} onPress={callback}>
-          <Text style={tw`text-center font-bold text-white my-auto text-2xl`}>
-            {on ? "Save" : "+" }
-          </Text>
+      {/* Create a new note on button press */}
+      <TouchableOpacity onPress={() => { addNote({ title: "New Note", content:  "" }); }} style={tw`bg-blue-500 rounded-full absolute bottom-[5%] right-8 items-center justify-center w-12 h-12`}>
+        <Text style={tw`text-white text-3xl`}>+</Text>
       </TouchableOpacity>
     </View>
-
-    </View>
-  )
+  );
 }
 
-function NewNoteScreen(){
+function EditScreen({ route, navigation }) {
+  const { data } = route.params;
+  const [title, setNoteTitle] = useState(data.title); // update title value
+  const [content, setNoteContent] = useState(data.content); // update note content etc
+  const [updateNote] = useUpdateNoteMutation();
+  const [deleteNote] =  useDeleteNoteMutation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: title,
+      headerRight: () => ( // right side of screen delete button for selected note
+        <TouchableOpacity onPress={() => deleteNoteAndGoBack()}>
+        <Text style={tw`mr-7 text-3xl`}>🗑️</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [title]);
+
+  const saveCurrentNote = () => { // save current note
+    updateNote({ // update the note (update properties)
+      id: data.id,
+      title: title,
+      content:  content,
+    }).then(() => { // after note
+      navigation.navigate("Home");
+    
+    });
+  };
+
+  const deleteNoteAndGoBack = () => {
+    deleteNote({ // delete current note & its data
+      id:  data.id,
+    }).then(() => {
+      navigation.navigate("Home"); // navigate back to home page
+    });
+  };
+
+  useEffect(() => { // when screen is unfocused, save current note
+    const unsubscribe  = navigation.addListener('blur', saveCurrentNote);
+    return unsubscribe;
+  }, [title, content]);
+
+
+// Editing note page
   return (
-    <View style={tw`bg-white w-full h-10`}>
-      
+    <View style={tw`flex-1 p-4 bg-gray-900 `}>
+      <TextInput
+        style={tw`w-full bg-gray-800 text-white p-2 mb-2 rounded`}
+        placeholder="Title"
+        value= {title}
+        onChangeText={setNoteTitle} // update note title
+      />
+      <TextInput
+        style={tw`w-full bg-gray-800 p-2  text-white rounded mb-2`}
+        placeholder="Enter your note..."
+        value= {content}
+        onChangeText={setNoteContent} // update note contnet
+        multiline 
+      /> 
+      <TouchableOpacity onPress={saveCurrentNote} style={tw`bg-blue-500 rounded p-2 mt-2  items-center justify-center`}>
+        <Text style={tw`text-white text-lg`}>Save</Text> 
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-function ExistingNoteScreen(){
+  const Stack = createNativeStackNavigator();
 
-}
-
-function App() {
+export default function App() {
   useDeviceContext(tw);
-
-  const [text, setText] = useState();
-
-
-  
 
   return (
     <Provider store={store}>
-      <SafeAreaView style={tw`bg-gray-900 flex-1`}>
-      
-      {HomeScreen()}
-      
-        
-
-        
-
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home"> 
+          <Stack.Screen
+            options={{ // Default home screen properties
+              headerStyle: tw`bg-gray-900`,
+              headerTitleAlign: 'center',
+              headerTintColor: 'white',
+              headerTitleStyle: tw`font-bold text-4xl`
+            } }
+            name="Notes" 
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{ // Editing page screen properties
+              headerStyle: tw`bg-gray-900 border-0`,
+              headerTintColor: 'white',
+              headerShadowVisible: false, 
+              headerTitleStyle: tw`font-bold`
+            }}
+            name="Edit"
+            component={EditScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
-  )
+  );
 }
-
-export default App;
-
-
-
-/* DEFAULT CODE
-import { SafeAreaView, Text } from 'react-native';
-import tw, { useDeviceContext } from 'twrnc';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import 'react-native-reanimated'; 
-
-function App() {
-  useDeviceContext(tw);
-
-  return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <Text style={tw`w-screen mt-16 text-center text-xl`}>
-          Hello World
-        </Text>
-      </SafeAreaView>
-    </Provider>
-  )
-}
-
-export default App;
-
-
-
-
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-
-
-
-*/
